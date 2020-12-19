83736470663d4f7c1334fac4f895eb92d66620e5
Bach <doanxuanbach132@gmai.com>
2020-12-19 22:20:29 +0700

    add login and sign-up(not add font)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const handlebars = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,8 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  }));
 
-app.use('/', indexRouter);
+app.use('/', usersRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', (req, res) => {
+  res.render('user/login', {layout : 'layouts/user'});
+});
+
+router.get('/sign-up', (req, res) => {
+  res.render('user/signup', {layout : 'layouts/user'});
+});
+
 module.exports = router;
