569b0b278fbcf3381de21e9ae100e85d95119874
Bach <doanxuanbach132@gmai.com>
2021-01-07 19:05:01 +0700

    full view

--- routes/GroupRoute.js
+++ routes/GroupRoute.js
@@ -5,7 +5,28 @@ router.get('/', (req, res) => {
     if(!req.user){
         res.redirect('/login');
     }
-    res.render('groupView', null);
+    res.render('Group/groupView', null);
+});
+
+router.get('/add', (req, res) => {
+    if(!req.user){
+        res.redirect('/login');
+    }
+    res.render('Group/groupAdd', null);
+});
+
+router.get('/edit', (req, res) => {
+    if(!req.user){
+        res.redirect('/login');
+    }
+    res.render('Group/groupEdit', null);
+});
+
+router.get('/detail', (req, res) => {
+    if(!req.user){
+        res.redirect('/login');
+    }
+    res.render('Group/groupDetail', null);
 });
 
 module.exports = router;
