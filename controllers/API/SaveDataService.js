1b22656a85c3d52a8596be1691e0a390e8700768
kevinpro2000 <kevinpro2000@gmail.com>
2021-01-09 20:43:23 +0700

    minor change

--- controllers/API/SaveDataService.js
+++ controllers/API/SaveDataService.js
@@ -1,8 +1,5 @@
 const noteModel = require('../../model/NoteModel');
 
 exports.Save = async(req, res, next) => {
-    console.log(req.query.noteID);
-    console.log(req.query.title);
-    console.log(req.query.content);
     res.json(await noteModel.saveNote(req.user._id, req.query.noteID, req.query.title, req.query.content));
 }
