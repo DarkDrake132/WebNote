2e537118b13e21b880dad17d844e2b9d920ab816
ducnghu <55783340+ducnghu@users.noreply.github.com>
2021-01-12 12:07:20 +0700

    add personal event

--- model/TaskModel.js
+++ model/TaskModel.js
@@ -1,6 +1,39 @@
 
 const {db} = require('../database/db');
-exports.AddTask = (task) => {
+const {ObjectId} = require('mongodb');
+
+exports.AddTask = async (id, task, type) => {
     const taskCollection = db().collection('task');
-    taskCollection.insert(task);
+    taskCollection.findOne({userid: id}).then(user => {
+        const newevent = {
+            event: task,
+            type: type,
+        }
+        if (user){
+            var newevents = user.events;
+            newevents.push(newevent);
+            taskCollection.updateOne({ userid: id }
+                , { $set: { events : newevents } });
+        }
+        else {
+            const newuser = {
+                userid: id,
+                events:[newevent,],
+            }
+            taskCollection.insert(newuser);
+        }
+    });
+
+};
+
+exports.GetTasks = async (id) => {
+    const taskCollection = db().collection('task');
+    let user = await taskCollection.findOne({userid: id});
+        return user.events;
+        if (user){
+            return user.events;
+        }
+        else{
+            return null;
+        }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var taskModel = require('../model/TaskModel')
 
 const {db} = require('../database/db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   if(!req.user){
     res.redirect('/login');
   }
-  const userCollection = db().collection('user');
-  res.render('index', { title: 'Express' });
+  let events = await taskModel.GetTasks(req.user._id);
+  res.render('index', { title: 'Express', events: events });
 });
 
+router.post('/', function(req, res, next) {
+  if(!req.user){
+    res.redirect('/login');
+  }
+  const taskCollection = db().collection('task');
+  taskModel.AddTask(req.user._id, req.body.newevent, req.body.neweventclass)
+  res.redirect('back');
+});
 module.exports = router;
